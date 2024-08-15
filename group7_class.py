826c2615ac3ecb5a9d3a1f01d20c6bfb8204d244
Theto <thetodongallo@gmail.com>
2024-08-15 13:56:01 -0600

    adding the group7_class.py file with class named Doctor

---
+++ group7_class.py
@@ -0,0 +1,14 @@
+
+#Class Doctor
+class Doctor:
+    def __init__(self, doctor_id, name, specialization, working_time, qualification, room_number):
+        self.doctor_id = doctor_id
+        self.name = name
+        self.specialization = specialization
+        self.working_time = working_time
+        self.qualification = qualification
+        self.room_number = room_number
+
+    def __str__(self):
+        return f"{self.doctor_id}_{self.name}_{self.specialization}_{self.working_time}_{self.qualification}_{self.room_number}"
+        
